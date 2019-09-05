4dfd0bdecacbf57bc5d4fa951cdf86db7ddc812a
yuandong <yuand@drplant.com.cn>
2019-09-05 18:26:08 +0800

    提交多线程抢单的service和一个异常日志测试类

---
+++ src/main/java/com/zhouyuan/rabbit/demo/service/InitService.java
@@ -0,0 +1,43 @@
+package com.zhouyuan.rabbit.demo.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CountDownLatch;
+
+public class InitService {
+
+    private static int MOBILE = 0;
+    private static final int MAX_THREAD_NUM = 500000;
+    private static final Logger log = LoggerFactory.getLogger(InitService.class);
+
+    public void generateMultiThread(){
+
+        log.info("开始初始化线程数：----> ");
+
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        for (int i = 0; i < MAX_THREAD_NUM; i++) {
+            new Thread(new ThreadService(countDownLatch)).start();
+        }
+        //启动多个线程
+        countDownLatch.countDown();
+    }
+
+    class ThreadService implements Runnable{
+
+        private CountDownLatch latch;
+
+        ThreadService(CountDownLatch latch){
+            this.latch = latch;
+        }
+        @Override
+        public void run() {
+            try {
+                latch.await();
+                MOBILE += 1;
+            } catch (InterruptedException e) {
+                log.error("线程运行中出现异常：",e);
+            }
+        }
+    }
+}
---
+++ src/test/java/com/zhouyuan/rabbit/demo/ExceptionTest.java
@@ -0,0 +1,32 @@
+package com.zhouyuan.rabbit.demo;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ExceptionTest extends DemoApplicationTests {
+    private static final Logger log = LoggerFactory.getLogger(ExceptionTest.class);
+    @Test
+    public void exceptionPrintTest(){
+        try {
+
+            int i = 10;
+            log.debug("{}被零整除会报异常",i);
+            i = i/0;
+        } catch (Exception e) {
+            log.error("异常打印到日志：",e);
+            log.error("e.fillInStackTrace方法测试：",e.fillInStackTrace());
+        }
+
+    }
+
+    @Test
+    public void logTest(){
+        Map<String,String> param = new HashMap<>(3);
+        param.put("TradeType","GetMemberInfo");
+        log.info("log直接打印map测试：param：{}",param);
+    }
+}
