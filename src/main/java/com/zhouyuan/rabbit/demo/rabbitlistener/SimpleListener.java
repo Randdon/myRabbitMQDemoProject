4af84db979667479d55fc5a909ac3198a07ebc14
yuandong <yuand@drplant.com.cn>
2019-09-23 18:11:33 +0800

    补提简单消息监听确认机制单元测试代码

--- src/main/java/com/zhouyuan/rabbit/demo/rabbitlistener/SimpleListener.java
+++ src/main/java/com/zhouyuan/rabbit/demo/rabbitlistener/SimpleListener.java
@@ -8,9 +8,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.listener.api.ChannelAwareMessageListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Component("simpleListener")
 public class SimpleListener implements ChannelAwareMessageListener {
 
     @Autowired
@@ -26,9 +28,11 @@ public class SimpleListener implements ChannelAwareMessageListener {
         try {
             byte[] msg = message.getBody();
             Product product = objectMapper.readValue(msg,Product.class);
+
+            //int exception = 1/0;
             logger.info("简单消息监听确认机制监听到消息：{}",product);
             channel.basicAck(tag,true);
-        } catch (IOException e) {
+        } catch (Exception e) {
             logger.error("简单消息监听确认机制发生异常：{}",e);
             //第二个Boolean参数的意义为是否重入队列
             channel.basicReject(tag,false);
--- src/test/java/com/zhouyuan/rabbit/demo/rabbitspringboot/SimpleListenerTest.java
+++ src/test/java/com/zhouyuan/rabbit/demo/rabbitspringboot/SimpleListenerTest.java
@@ -1,22 +1,44 @@
 package com.zhouyuan.rabbit.demo.rabbitspringboot;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zhouyuan.rabbit.demo.DemoApplicationTests;
+import com.zhouyuan.rabbit.demo.entity.Product;
 import org.junit.Test;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageBuilder;
+import org.springframework.amqp.core.MessageDeliveryMode;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 
+/**
+ * 简单消息监听确认机制测试类
+ */
 public class SimpleListenerTest extends DemoApplicationTests {
 
     @Autowired
     RabbitTemplate rabbitTemplate;
     @Autowired
     Environment env;
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Test
     public void simpleListenerTest(){
         rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
-        rabbitTemplate.setExchange(env.getProperty("rabbitmq.simple.container.routingKey.name"));
+        rabbitTemplate.setExchange(env.getProperty("rabbitmq.simple.container.exchange.name"));
+        rabbitTemplate.setRoutingKey(env.getProperty("rabbitmq.simple.container.routingKey.name"));
+
+        Product product = new Product(1,"simpleListenerTest",20);
+        try {
+            Message message = MessageBuilder.withBody(objectMapper.writeValueAsBytes(product))
+                    .setDeliveryMode(MessageDeliveryMode.PERSISTENT)
+                    .build();
+            rabbitTemplate.convertAndSend(message);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
 }
